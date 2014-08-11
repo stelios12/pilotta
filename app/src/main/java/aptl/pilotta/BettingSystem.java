cd0da47fd4917433fce7426d8bbf05ad450ab9fe
Stelios <shartoutsios@gmail.com>
2014-08-11 17:10:24 +0300

    xml file for betting

---
+++ app/src/main/java/aptl/pilotta/BettingSystem.java
@@ -0,0 +1,13 @@
+package aptl.pilotta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BettingSystem extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bet_layout);
+    }
+}
