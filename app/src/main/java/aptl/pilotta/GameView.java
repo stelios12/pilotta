bd35ce85380c29c7b8d9a1ee2bd728ff8707426d
constantinos <cv1212@imperial.ac.uk>
2014-08-13 14:41:08 +0300

    Sorting of cards + optimisations

--- app/src/main/java/aptl/pilotta/GameView.java
+++ app/src/main/java/aptl/pilotta/GameView.java
@@ -4,15 +4,14 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.util.DisplayMetrics;
 import android.view.SurfaceView;
-
 import java.util.ArrayList;
-import java.util.ResourceBundle;
+import java.util.Collections;
+import java.util.Comparator;
 
-import aptl.pilotta.GameActivity;
-import aptl.pilotta.R;
 import aptl.pilotta.game.deck.Card;
+import aptl.pilotta.game.deck.Kind;
+import aptl.pilotta.game.deck.Value;
 import aptl.pilotta.game.utils.Utils;
 
 /**
@@ -21,19 +20,25 @@ import aptl.pilotta.game.utils.Utils;
 public class GameView extends SurfaceView {
 
     private ArrayList<Card> cards;
+    private static CardSorter cardSorter;
     private static Bitmap[] cardRes = new Bitmap[32];
 
     public GameView(Context context) {
         super(context);
         cards = new ArrayList<Card>();
+        cardSorter = new CardSorter();
 
         for (int i = 0; i < cardRes.length; i++) {
             cardRes[i] = BitmapFactory.decodeResource(getResources(), Utils.imageResources[i]);
+            cardRes[i] = Bitmap.createScaledBitmap(cardRes[i], 150, 200, false);
         }
+
     }
 
     public void addCard(Card card) {
         cards.add(card);
+        Collections.sort(cards,cardSorter);
+
     }
 
     @Override
@@ -44,9 +49,53 @@ public class GameView extends SurfaceView {
         int offset = width/10;
         for (Card c : cards) {
             Bitmap map = cardRes[c.getImageRes()];
-            Bitmap resized = Bitmap.createScaledBitmap(map, 150, 200, false);
-            canvas.drawBitmap(resized,offset,0,null);
+            canvas.drawBitmap(map,offset,0,null);
             offset += width/11;
+        }
+
+    }
+
+    private class CardSorter implements Comparator<Card> {
+
+        @Override
+        public int compare(Card lhs, Card rhs) {
+
+            Kind k1 = lhs.getKind();
+            Kind k2 = rhs.getKind();
+            Value v1 = lhs.getValue();
+            Value v2 = rhs.getValue();
+
+            if (sortCode(k1) < sortCode(k2)) {
+                return -1;
+            } else if (sortCode(k1) == sortCode(k2)) {
+                if (v1.hashCode() > v2.hashCode()) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            } else {
+                return 1;
+            }
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            return false;
+        }
+
+        private int sortCode(Kind k) {
+            switch (k) {
+                case CLUBS:
+                    return 1;
+                case HEARTS:
+                    return 2;
+                case SPADES:
+                    return 3;
+                case DIAMONDS:
+                    return 4;
+                default:
+                    return 5;
+            }
         }
 
     }
--- app/src/main/java/aptl/pilotta/game/deck/Card.java
+++ app/src/main/java/aptl/pilotta/game/deck/Card.java
@@ -30,6 +30,28 @@ public class Card implements Serializable {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Card card = (Card) o;
+
+        if (imageRes != card.imageRes) return false;
+        if (kind != card.kind) return false;
+        if (value != card.value) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = kind.hashCode();
+        result = 31 * result + value.hashCode();
+        result = 31 * result + imageRes;
+        return result;
+    }
+
+    @Override
     public String toString() {
         return value.toString() + " of " + kind.toString();
     }
