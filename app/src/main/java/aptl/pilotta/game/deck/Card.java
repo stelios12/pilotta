e605aee10275fd485c19214ec52275e87720ae8c
constantinos <cv1212@imperial.ac.uk>
2014-08-13 14:44:04 +0300

    Bug fix

--- app/src/main/java/aptl/pilotta/game/deck/Card.java
+++ app/src/main/java/aptl/pilotta/game/deck/Card.java
@@ -32,7 +32,7 @@ public class Card implements Serializable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || ((Object)this).getClass() != o.getClass()) return false;
 
         Card card = (Card) o;
 
