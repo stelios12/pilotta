fb4216effd116749ddf7241ada45675ce96e115e
Stelios <shartoutsios@gmail.com>
2014-08-14 09:00:29 +0300

    xml file for betting

--- app/src/main/java/aptl/pilotta/game/betsystem/BetSystem.java
+++ app/src/main/java/aptl/pilotta/game/betsystem/BetSystem.java
@@ -1,7 +1,20 @@
 package aptl.pilotta.game.betsystem;
 
-/**
- * Created by constantinos on 09/08/2014.
- */
-public class BetSystem {
+import aptl.pilotta.game.deck.Kind;
+
+
+public class BetSystem  {
+
+    private int userId;
+    private int betNumber;
+    private Kind kind;
+
+    protected BetSystem(int userId, int betNumber, Kind kind){
+        this.userId=userId;
+        this.betNumber=betNumber;
+        this.kind=kind;
+    }
+
+
+
 }
